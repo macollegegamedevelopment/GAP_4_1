b9b691fa6f55009bc45ac771be440e811c948551
Berend Weij <info@berendweij.nl>
2019-05-13 08:23:13 +0200

    removed some comments

--- inventory_system/Assets/Scripts/Test.cs
+++ inventory_system/Assets/Scripts/Test.cs
@@ -5,20 +5,11 @@ using UnityEngine;
 public class Test : MonoBehaviour {
 
     /*
-    1:
     Van een InventorySystem verwacht je dat hij bepaalde logica heeft
     zoals het toevoegen van items
     en dat je items er weer uit kunt halen
     Dat je het aantal items kunt opvragen van een bepaald type, etc.
-    2: doorlopen TestBad()
-        // stel we hebben een inventory system
-        // en ik wil een steen kunnen gooien. Dan heb ik een steen nodig eerst.
-    3: doorlopen TestGood()
-    4: doorlopen verbeteringen InventoryGood
-    5: StateMachine doornemen RIP
     */
-
-	// Use this for initialization
 	void Start ()
 	{
 	    TestBad();
